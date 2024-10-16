920e5d1eb46d206b7ccd8629945d18aa387ee563
fiqrioemry <foemry@gmail.com>
2024-10-16 23:47:09 +0700

    add section category list on homepage

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { useDispatch } from "react-redux";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  const dispatch = useDispatch();
+  return (
+    <section className="py-16">
+      <div className="container mx-auto">
+        {/* title */}
+        <div>
+          <div className="flex flex-row items-center gap-4 text-tertiary font-semibold mb-5">
+            <div className="w-5 h-10 rounded-md bg-tertiary"></div>
+            <h4>This Month</h4>
+          </div>
+          <div className="flex flex-row justify-between items-center">
+            <h1>Browse By Category</h1>
+            <div className="flex gap-x-4">
+              <div className="p-3 rounded-full bg-secondary/20 cursor-pointer">
+                <FaArrowLeft className="text-xl block" />
+              </div>
+              <div className="p-3 rounded-full  bg-secondary/20 cursor-pointer">
+                <FaArrowRight className="text-xl block" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* card */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-4"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import BestProduct from "../components/BestProduct";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
     <section>
       <Banner />
       <BestProduct />
+      <Categories />
     </section>
   );
 };
