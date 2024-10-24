9d321acf5c6064073d30556ceef5687ea73c54ca
fiqrioemry <foemry@gmail.com>
2024-10-24 23:54:48 +0700

    add logout function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ import CartElement from "../elements/header/CartElement";
 import NavMenuElement from "../elements/header/NavMenuElement";
 import SearchInputElement from "../elements/header/SearchInputElement";
 import { useNavigate } from "react-router-dom";
+import UserProfileElement from "../elements/header/UserProfileElement";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -104,24 +105,7 @@ const Header = () => {
                     <FaRegUserCircle />
                   </button>
                   {profileOpen && (
-                    <div className="absolute border top-8 right-0 bg-white shadow-xl rounded-md px-2 py-2 space-y-2 z-20">
-                      <div className="w-full min-w-[160px] space-y-2 flex flex-col justify-center">
-                        <div className="text-lg font-medium">
-                          Welcome: emilys
-                        </div>
-                        <button className="flex items-center gap-x-2 py-2 px-2 rounded-md hover:bg-gray-300/50 w-full">
-                          <MdHome className="text-2xl" />
-                          Profile
-                        </button>
-                        <button
-                          onClick={handleLogout}
-                          className="flex items-center gap-x-2 py-2 px-2 rounded-md hover:bg-gray-300/50 w-full"
-                        >
-                          <MdLogout className="text-2xl" />
-                          Logout
-                        </button>
-                      </div>
-                    </div>
+                    <UserProfileElement handleLogout={handleLogout} />
                   )}
                 </div>
               )}
--- src/elements/header/UserProfileElement.jsx
+++ src/elements/header/UserProfileElement.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 import { MdLogout, MdHome } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
 
-const UserProfileElement = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    Cookies.remove("token");
-    Cookies.remove("user");
-    navigate("/signin");
-  };
-
+const UserProfileElement = ({ handleLogout }) => {
   return (
     <div className="absolute border top-8 right-0 bg-white shadow-xl rounded-md px-2 py-2 space-y-2 z-20">
       <div className="w-full min-w-[160px] space-y-2 flex flex-col justify-center">
